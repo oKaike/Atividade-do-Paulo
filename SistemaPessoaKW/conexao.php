03054870aefbbe538b9bbb8e48b44861eb20559e
Kaike Correia <148728784+oKaike@users.noreply.github.com>
2025-10-31 11:56:51 -0300

    Add files via upload

---
+++ SistemaPessoaKW/conexao.php
@@ -0,0 +1,12 @@
+<?php
+    class Conexao{
+       private static $instancia;
+       public static function pegarConexao(){
+            if(!isset(self::$instancia)) {
+                self::$instancia = new PDO(
+                    "mysql:host=localhost; dbname=cadastroP; charset=utf8", "root", "");
+            }
+            return self::$instancia;   
+       } 
+    }
+?>
