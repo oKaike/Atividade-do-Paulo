8bfb29c47919ea0d48fab7eed76abd51179d73cb
oKaike <mesmekkaike@gmail.com>
2026-06-20 14:15:42 -0300

    Novas alterações

--- SistemaPessoaKW2/JS/script.js
+++ SistemaPessoaKW2/JS/script.js
@@ -6,22 +6,56 @@ function funcCadastrar(){
 
     document.querySelector('p').innerHTML = nome;
 
-    fetch('https://localhost/SistemaDeAgendamentos/PHP/modeloPessoa.php',{
+    fetch('https://localhost/SistemaDeAgendamentos/SistemaPessoaKW2/PHP/modeloPessoa.php',{
         method: "POST",
         headers:{
             'Content-Type': 'application/json',
         },
         body:JSON.stringify({
             Nome : nome,
-            cpf : cpf,
-            contato : contato,
-            senha : senha
+            Cpf : cpf,
+            Contato : contato,
+            Senha : senha
         }),
     })
     .then(response => response.json())
     .then(data =>{JSON.stringify({
         Logado : true,
         Nome : data.nome
-    })} )
+    })})
+}
 
+function funcConsultarInformacoesCadastrais(){
+    let cpf = document.getElementById('cpf').value;
+    
+    fetch("https://localhost/SistemaDeAgendamentos/SistemaPessoaKW2/PHP/modeloPessoa.php", {
+        method: "GET",
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify({
+            Cpf :cpf
+        })
+
+    })
+    .then(response => response.json())
+    .then(data =>{JSON.stringify({
+        Logado: true,
+        id : data.id
+    })})
+}
+
+function funcAtualizar(){
+    let nome;
+    let contato;
+    fetch("https://localhost/SistemaDeAgendamentos/SistemaPessoaKW2/PHP/modeloPessoa.php",{
+        method: "PUT",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            Nome: nome,
+            Contato: contato
+        })
+    })
 }
--- SistemaPessoaKW2/PHP/modeloPessoa.php
+++ SistemaPessoaKW2/PHP/modeloPessoa.php
@@ -1,20 +1,23 @@
 <?php
-$dados = file_get_contents('../JS/script.js');
-echo $dados;
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    // Criar Usuario
-    echo "Por padrão estamos usando: ". $_SERVER['REQUEST_METHOD'];
-}
-else if($_SERVER['REQUEST_METHOD'] == "GET"){
-    // Consulta de Usuario
+class modeloPessoa{
+//  public $dados = file_get_contents('../JS/script.js');
+// echo $dados;
+ public function RequestMetodos(){
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        // Criar Usuario
+        echo "Estamos usando metodo de requisição: ". $_SERVER['REQUEST_METHOD'];
+    }
+    else if($_SERVER['REQUEST_METHOD'] == "GET"){
+        // Consulta de Usuario
+        echo "Por padrão estamos usando: ". $_SERVER['REQUEST_METHOD'];
+    }
+    else if($_SERVER['REQUEST_METHOD'] == "PUT"){
+        // Atualizar dados do usuario
 
-}
-else if($_SERVER['REQUEST_METHOD'] == "PUT"){
-    // Atualizar dados do usuario
+    }
+    else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+        // Deletar daods do usuario
 
-}
-else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
-    // Deletar daods do usuario
-
+    }}
 }
 ?>
